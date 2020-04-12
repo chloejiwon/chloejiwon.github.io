18bb8eed175e961a89b6ea2f4d8be624b79531d4
Jiwon <chloe326o.o@gmail.com>
2020-04-13 00:18:02 +0900

    about page changed

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "github.io 블로그 시작하기",
-        "excerpt":" ","categories": ["Blog"],
+        "excerpt":"github.io 블로그를 시작하기로 했다..   이것저것 만지니까 꽤 재밌잖아!?!?!   심지어 좀 귀여운듯 🙄   ","categories": ["Blog"],
         "tags": ["Blog"],
         "url": "http://localhost:4000/blog/first-post/",
         "teaser":null}]
