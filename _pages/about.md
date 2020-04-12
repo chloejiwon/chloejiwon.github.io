d6ad81cbf280d9e4b874e40ae3c6fc67ce8835ed
Jiwon <chloe326o.o@gmail.com>
2020-04-12 18:14:07 +0900

    1st baseline

---
+++ _pages/about.md
@@ -0,0 +1,10 @@
+---
+title: "이 블로그 설명서"
+permalink: /about/
+layout: single
+---
+
+##  블로그
+
+chloejiwon.github.io
+블로그는 기술과 관련 잡다한 것들을 기록하는 블로그다.
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "github.io 블로그 시작하기",
+        "excerpt":" ","categories": ["Blog"],
+        "tags": ["Blog"],
+        "url": "http://localhost:4000/blog/first-post/",
+        "teaser":null}]
