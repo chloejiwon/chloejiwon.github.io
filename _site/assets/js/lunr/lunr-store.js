57e927f624097a9428fb4f2c00341245bc769690
chloejiwon <chloe326o.o@gmail.com>
2021-02-03 11:13:07 +0900

    Post BERT post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -35,7 +35,7 @@ var store = [{
         "url": "http://localhost:4000/recommend%20system/matrix-factorization/",
         "teaser":null},{
         "title": "WebRTC로 실시간 영상 통화를 하며 Object Detection을 해보자 - 1탄",
-        "excerpt":"프로젝트 진행하면서 처음 접한 webrtc.. 실시간으로 영상 채팅하며tensorflow js 모델과 함께 객체 인식을 하는 기능을 구현해보는데… 👀 왕왕 초보라 틀린 정보가 있을 수 있음을 미리 고지합니다 📌 Table of Contents WebRTC가 뭐냐? webrtc 동작방식 WebRTC구현하기 N:N영상통화 Tensorflow js모델을 심자 reference 🚦 webRTC가 뭐냐? WebRTC는 web application간에 실시간 통신 기능을 제공하는...","categories": ["웹, 인공지능"],
+        "excerpt":"프로젝트 진행하면서 처음 접한 webrtc.. 실시간으로 영상 채팅하며tensorflow js 모델과 함께 객체 인식을 하는 기능을 구현해보는데… 👀 왕왕 초보라 틀린 정보가 있을 수 있음을 미리 고지합니다 📌 Table of Contents WebRTC가 뭐냐? webrtc 동작방식 WebRTC구현하기 N:N영상통화 reference webRTC가 뭐냐? WebRTC는 web application간에 실시간 통신 기능을 제공하는 기술. 특히 마이크나 스피커,...","categories": ["웹, 인공지능"],
         "tags": ["tensorflow, javascript"],
         "url": "http://localhost:4000/%EC%9B%B9,%20%EC%9D%B8%EA%B3%B5%EC%A7%80%EB%8A%A5/webrtc+tensorflow/",
         "teaser":null},{
@@ -43,4 +43,49 @@ var store = [{
         "excerpt":"ibm CLouders로 활동하면서 ibm CLoud Essentials 강의 들으며 note taking한 거 공유하는 글이다. 클라우드의 개략적인 개념을 이해할 수 있어서 좋았다. Iaas / Paas / Saas aaS = as a Service, how you consume Iaas : Infrastructure. For example, my computer, some random other person’s computer running somewhere else … persona...","categories": ["클라우드"],
         "tags": ["cloud"],
         "url": "http://localhost:4000/%ED%81%B4%EB%9D%BC%EC%9A%B0%EB%93%9C/ibm-clouders-notetaking/",
+        "teaser":null},{
+        "title": "WebRTC로 실시간 영상 통화를 하며 Object Detection을 해보자 - 2탄",
+        "excerpt":"프로젝트 진행하면서 처음 접한 webrtc.. 실시간으로 영상 채팅하며tensorflow js 모델과 함께 객체 인식을 하는 기능을 구현해보는데… 👀 2탄. 대망의 tensorflow js 모델로 object detection 성공해보자 왕왕 초보라 틀린 정보가 있을 수 있음을 미리 고지합니다 📌 Table of Contents Tensorflow js모델을 심자 Tensorflow js WebRTC + Tensorflow.JS Tensorflowjs 모듈 설치 reference...","categories": ["웹, 인공지능"],
+        "tags": ["tensorflow, javascript"],
+        "url": "http://localhost:4000/%EC%9B%B9,%20%EC%9D%B8%EA%B3%B5%EC%A7%80%EB%8A%A5/webrtc+tensorflow2/",
+        "teaser":null},{
+        "title": "IBM Cloud Foundry 로 node js 간단 배포하기",
+        "excerpt":"webRTC 프로젝트 시작하기 전, node js로 간단한 webrtc 프로그램을 만들고 ibm cloud foundry로 배포해봤습니다. cli로 뚝딱 되어버려 너무 신기해요.🤔 📌 Table of Contents 서론 IBM Cloud Foundry란? IBM Cloud Foundry 로 배포 reference 서론 이 글은 간단한 WebRTC Web App을 IBM Cloud Foundry를 사용하여 배포해보고자 한다. 생각보다 매우 간단! 그리고...","categories": ["웹"],
+        "tags": ["node js, ibm cloud"],
+        "url": "http://localhost:4000/%EC%9B%B9/ibm-cloudfoundry/",
+        "teaser":null},{
+        "title": "IBM Watson을 이용한 간단한 NLP 실습",
+        "excerpt":"IBM Watson Studio 를 이용해서 간단한 NLP 프로그램을 만들어봤습니다. 자연어 처리 한번 해보고 싶었는데, 자연어 처리하면 또 IBM Watson 아니겠어요! NLP에 대한 이해는 아주 적지만 친절한 외쿡 블로그를 따라 하나씩 작성해보았습니다. 참고한 친절한 외쿡 블로그 가기 과연 성공할런지.. 👶🏽 📌 Table of Contents 뭐 할건가요? 실습 시작 뭐 할건가요? Review...","categories": ["NLP"],
+        "tags": ["NLP, ibmcloud"],
+        "url": "http://localhost:4000/nlp/ibm-nlp-for-beginners/",
+        "teaser":null},{
+        "title": "IBM Cloud - auto AI 실습",
+        "excerpt":"Auto AI 라고 하는 IBM Cloud watson이 선보이는 AutoML 서비스의 개념에 대해 알아보고, 사용 해서 배포까지 해보도록 합니다. 😎 전체적인 모든 내용은 아주 친절한 유투브 를 참고했습니다. 준비물 IBM Cloud 계정 Dataset ML이니 Dataset이 필요하겠죠? 무료인 kaggle에서 제공된 dataset을 이용해봅니다. 아래 링크에서 insurance.csv 파일을 이용할 겁니다. 여러 feature를 사용해서 보험...","categories": ["ibmcloud"],
+        "tags": ["autoML"],
+        "url": "http://localhost:4000/ibmcloud/ibmcloud-autoai/",
+        "teaser":null},{
+        "title": "python의 process와 thread",
+        "excerpt":"python 의 병렬 처리, process와 thread에 대해 알아보자. 그리고 python의 특별한 GIL도.. 🙄 Process 와 Thread 아주 기초적인 process와 thread의 차이점은 다음과 같다. process : process란 메모리에 적재되어(load) 실행되고 있는 프로그램이다. thread : Thread는 process내에서 자원, memory map을 공유하며 동작하는 흐름의 단위이다. 자원을 공유하기 때문에 race condition과 같은 상황(=예를 들어...","categories": ["python"],
+        "tags": ["python"],
+        "url": "http://localhost:4000/python/python-thread-process/",
+        "teaser":null},{
+        "title": "🐈 Deep learning - CNN (1) ",
+        "excerpt":"딥 러닝 공부의 기록. CNN 기초를 다져 보자. (언제 RNN, transformer 등…. 하지? 막막🤧) 대부분의 내용은 [밑바닥부터 시작하는 딥러닝] 책을 바탕으로 하며 거의 요약본이라 할 수 있습니다. 서론 이 글에서는 딥 러닝의 기본적인 개념, Logistic Regression, 활성화 함수, 가중치 업데이트 등을 알고 있다고 가정하고 진행하도록 하겠다. CNN(Convolutional Neural Network)는 이미지...","categories": ["deeplearning"],
+        "tags": ["cnn"],
+        "url": "http://localhost:4000/deeplearning/deeplearning-cnn-1/",
+        "teaser":null},{
+        "title": "☁IBM Clouders 활동 후기(20/7 - 20/9)",
+        "excerpt":"IBM Clouder로 불성실하게 살아온지(?) 7~9월 어느덧 3개월 정도가 되었습니다. 🤧 그동안의 미션과 활동에 대해 되돌아 보며 주로 반성하는 시간을 갖도록 하겠습니다. 시작 IBM 에서 주관하는 Call For Code 2020에 참여하며 이 모든 것이 시작되었습니다. (거창..) 할 줄 하는 거 하나 없었는데 친구랑 해커톤 한번 나가보자 했는데 이렇게 스케일 큰 해커톤일...","categories": ["IBM_CLOUDERS"],
+        "tags": ["ibmcloud"],
+        "url": "http://localhost:4000/ibm_clouders/ibm-cloud-review/",
+        "teaser":null},{
+        "title": "Challenging Common Assumptions in the Unsupervised Learning of Disentangled Representations",
+        "excerpt":"2019년 Top 10 논문에 뽑힌 Common Assumptions in the unsupervised learning of disentangled representations 논문을 살펴보자. 이번 논문은 제목 그대로 Common Assumption에 대해 도전하는 논문이다. 그래서 이걸 이해하려면 애초에 그럼 내가 Common Assumption이 있을 정도로 배경 지식이 있어야 했기 때문에(…) 배경 지식을 (논문을 이해할 만큼만) 간단히 정리해보았다. 여기서 🥝 로...","categories": ["MachineLearning"],
+        "tags": ["머신러닝, 논문"],
+        "url": "http://localhost:4000/machinelearning/ml-researchpaper-1/",
+        "teaser":null},{
+        "title": "BERT: Pre-training of Deep Bidirectional Transformers for Language Understanding",
+        "excerpt":"이번엔 2019년도에 publish된 논문 BERT를 읽어보았다. 아무래도 attention 에 대한 지식이 거의 전무하다보니, 피상적으론 이해했으나 깊숙한 메커니즘은 이해하지 못한 것 같다. (논문이 쉽게 쓰여있어서 망정이지..) 하여튼, 다음 논문으로 attention is all you need를 읽어서 다시 싱크를 맞춰봐야겠다. Abstract BERT : New language representation model, Bidirectional Encoder Representation from Transformers 다른...","categories": ["MachineLearning"],
+        "tags": ["머신러닝, 논문"],
+        "url": "http://localhost:4000/machinelearning/ml-researchpaper-2/",
         "teaser":null}]
